596a75a77022839f0d5a0f8ffad76a5abbc2b4e7
YOGEESH K P <yogeesh793@gmail.com>
2024-12-22 17:28:19 +0530

    Add files via upload

---
+++ Problem_7.py
@@ -0,0 +1,8 @@
+"""Membership Operator: Write a python program that:
+* Takes a string as input from the user.
+* Checks if the letter 'a' in the string (using in)
+* Checks if the string doesn't contain the word "python"."""
+
+s=input("s: ")
+print("a" in s)
+print("python" not in s)
